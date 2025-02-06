605b1347b73f969a86e01970f5881945ec2fa25e
Muhammad Mominul Huque <mominul2082@gmail.com>
2025-02-06 18:47:58 +0600

    Dice roller app

---
+++ dice/lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice!',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Roll the Dice!'),
+    );
+  }
+}
+
+final random = Random();
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int currentDice = 1;
+
+  void _roll() {
+    setState(() {
+      currentDice = random.nextInt(6) + 1;
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/dice-$currentDice.png',
+              width: 200,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _roll, child: const Text('Roll Dice')),
+            SizedBox(height: 20),
+            const Text(
+              'You have this dice this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
